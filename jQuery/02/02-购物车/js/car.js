137b7a3e01e1e646903915226d1cdcb5e6975f28
Leo00008 <377041486@qq.com>
2022-06-19 18:39:03 +0800

    commit

--- jQuery/02/02-购物车/js/car.js
+++ jQuery/02/02-购物车/js/car.js
@@ -38,23 +38,112 @@ $(function () {
 
     //3.增删按钮
     $(".increment").click(function () {
+        //获取ipt里面数字的值
         var n = $(this).siblings(".itxt").val()
+        //每次点击 n+1
         n++;
+        if (n <= 0) n = 1
+        // 把n的值加给 ipt
         $(this).siblings('.itxt').val(n)
 
+        //  获取该商品的单价 
         var p = $(this).parent().parent().siblings(".p-price").html()
-        
-        $(".p-sum").text()
+        //单价为 ￥12.6  需要去除前面字符，所以字符截取  substr()  p=12.6
+        p = p.substr(1)
+        //$(this).parent().parent().siblings(".p-sum")总价  =   n*p   ipt的数字*单价
+        $(this).parents(".p-num").siblings(".p-sum").text('￥' + (n * p).toFixed(2))
+        getSum();
     })
 
     $(".decrement").click(function () {
+        //获取ipt里面数字的值
         var n = $(this).siblings(".itxt").val()
+        //每次点击 n+1
         n--;
+        //如果n<1,返回false，不再执行以下代码
         if (n < 1) return false
+        //把n赋值给 ipt
         $(this).siblings('.itxt').val(n)
-
-
+        //  获取该商品的单价  并且截取掉前面的￥
+        var p = $(this).parent().parent().siblings(".p-price").html()
+        p = p.substr(1)
+        //$(this).parent().parent().siblings(".p-sum")总价  =   n*p   ipt的数字*单价
+        $(this).parents(".p-num").siblings(".p-sum").text('￥' + (n * p).toFixed(2))
+        getSum()
     })
+
+    //如果用户修改ipt的值，对应的价格发生变化
+    $(".itxt").change(function () {
+
+        //获取ipt的值
+        var n = $(this).val();
+        //如果ipt的值等于小于0
+        if (n <= 0) {
+            //他的值重新为1
+            $(this).val("1")
+            //再次获取值
+            var n = $(this).val();
+            //获取单价
+            var p = $(this).parents('.p-num').siblings(".p-price").html()
+            //因为单价带￥，所以截取字符串
+            p = p.substr(1);
+            //总价
+            $(this).parents(".p-num").siblings('.p-sum').text((n * p).toFixed(2))
+        } else {           
+            //获取单价
+            var p = $(this).parents('.p-num').siblings(".p-price").html()
+            //因为单价带￥，所以截取字符串
+            p = p.substr(1);
+
+            $(this).parents(".p-num").siblings('.p-sum').text((n * p).toFixed(2))
+        }
+    })
+
+
+    //结算 //封装函数
+    //结算栏的商品数量要改变
+    //获取每个ipt的值，让他们相加。赋值给结算数量   
+    getSum();
+    function getSum() {
+        var count = 0;//计算总件数
+        var money = 0;//计算总金钱
+        $(".itxt").each(function (i, ele) {
+            count += parseInt($(ele).val())
+        })
+        $('.amount-sum em').text(count)
+
+        $(".p-sum").each(function (i, ele) {
+            money += parseFloat($(ele).text().substr(1))
+        })
+        $('.price-sum em').text('￥' + money.toFixed(2))
+    }
+
+    //删除购物车商品
+    $(".p-action").click(function () {
+        //每个删除按钮绑定事件，点击的时候删除自己的父元素
+        $(this).parents(".cart-item").remove()
+        getSum();
+    })
+    //清空购物车按钮
+    $(".clear-all").click(function () {
+        $(this).parents(".cart-floatbar").siblings(".cart-item-list").empty();
+        getSum();
+    })
+    //删除选中商品
+    $(".remove-batch").click(function () {
+        $(".j-checkbox").each(function () {
+            if ($(this).prop('checked') == true) {
+                $(this).parents(".cart-item").remove()
+            }
+        })
+        getSum();
+    })
+
+    //判断复选框是否被选中，计算总和
+
+
+
+
 
 
 
