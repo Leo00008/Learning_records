e3ef3db5e9549ee55a124a7bf987395e8a68c8c2
Leo00008 <377041486@qq.com>
2022-06-19 10:10:14 +0800

    cimmit

---
+++ jQuery/02/02-购物车/js/car.js
@@ -0,0 +1,61 @@
+$(function () {
+    // 1. 全选 全不选功能模块
+    // 就是把全选按钮（checkall）的状态赋值给 三个小的按钮（j-checkbox）就可以了
+    // 事件可以使用change
+    $(".checkall").change(function () {
+        // console.log($(this).prop("checked"));
+        $(".j-checkbox, .checkall").prop("checked", $(this).prop("checked"));
+        if ($(this).prop("checked")) {
+            // 让所有的商品添加 check-cart-item 类名
+            $(".cart-item").addClass("check-cart-item");
+        } else {
+            // check-cart-item 移除
+            $(".cart-item").removeClass("check-cart-item");
+        }
+    });
+    // 2. 如果小复选框被选中的个数等于3 就应该把全选按钮选上，否则全选按钮不选。
+    $(".j-checkbox").change(function () {
+        // if(被选中的小的复选框的个数 === 3) {
+        //     就要选中全选按钮
+        // } else {
+        //     不要选中全选按钮
+        // }
+        // console.log($(".j-checkbox:checked").length);
+        // $(".j-checkbox").length 这个是所有的小复选框的个数
+        if ($(".j-checkbox:checked").length === $(".j-checkbox").length) {
+            $(".checkall").prop("checked", true);
+        } else {
+            $(".checkall").prop("checked", false);
+        }
+        if ($(this).prop("checked")) {
+            // 让当前的商品添加 check-cart-item 类名
+            $(this).parents(".cart-item").addClass("check-cart-item");
+        } else {
+            // check-cart-item 移除
+            $(this).parents(".cart-item").removeClass("check-cart-item");
+        }
+    })
+
+    //3.增删按钮
+    $(".increment").click(function () {
+        var n = $(this).siblings(".itxt").val()
+        n++;
+        $(this).siblings('.itxt').val(n)
+
+        var p = $(this).parent().parent().siblings(".p-price").html()
+        
+        $(".p-sum").text()
+    })
+
+    $(".decrement").click(function () {
+        var n = $(this).siblings(".itxt").val()
+        n--;
+        if (n < 1) return false
+        $(this).siblings('.itxt').val(n)
+
+
+    })
+
+
+
+})
